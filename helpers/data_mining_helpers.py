5fbad0cf8041a6fdc8e9b7d70747cd1c79ac5e20
blackanana <nita19970913@gmail.com>
2023-10-26 02:18:19 +0800

    add function in dmh

--- helpers/data_mining_helpers.py
+++ helpers/data_mining_helpers.py
@@ -6,33 +6,47 @@ Author: Elvis Saravia
 Email: ellfae@gmail.com
 """
 
+
 def format_rows(docs):
-    """ format the text field and strip special characters """
+    """format the text field and strip special characters"""
     D = []
     for d in docs.data:
-        temp_d = " ".join(d.split("\n")).strip('\n\t')
+        temp_d = " ".join(d.split("\n")).strip("\n\t")
         D.append([temp_d])
     return D
 
+
 def format_labels(target, docs):
-    """ format the labels """
+    """format the labels"""
     return docs.target_names[target]
 
+
 def check_missing_values(row):
-    """ functions that check and verifies if there are missing values in dataframe """
+    """functions that check and verifies if there are missing values in dataframe"""
     counter = 0
     for element in row:
         if element == True:
-            counter+=1
+            counter += 1
     return ("The amoung of missing records is: ", counter)
+
+
+# for homework
+def check_missing_values_h(row):
+    """functions that check and verifies if there are missing values in dataframe"""
+    counter = 0
+    for element in row:
+        if element is None:
+            counter += 1
+    return ("The amoung of missing records is: ", counter)
+
 
 def tokenize_text(text, remove_stopwords=False):
     """
     Tokenize text using the nltk library
     """
     tokens = []
-    for d in nltk.sent_tokenize(text, language='english'):
-        for word in nltk.word_tokenize(d, language='english'):
+    for d in nltk.sent_tokenize(text, language="english"):
+        for word in nltk.word_tokenize(d, language="english"):
             # filters here
             tokens.append(word)
     return tokens
